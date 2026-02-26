a24d59339b9cf12d7734c52211e0f3a8ecc9beee
Zane Helton <zane99@me.com>
2026-02-25 19:10:01 -0500

    Add basic class template for ring buffer

--- include/ring_buffer/ring_buffer.h
+++ include/ring_buffer/ring_buffer.h
@@ -1,1 +1,16 @@
-// Intentionally blank
+#ifndef INCLUDE_RING_BUFFER_RING_BUFFER_H_
+#define INCLUDE_RING_BUFFER_RING_BUFFER_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace dsa {
+template <typename T, uint64_t capacity = 0>
+class RingBuffer {
+ public:
+  RingBuffer() { std::cout << "Constructor called\n"; }
+  ~RingBuffer() { std::cout << "Destructor called\n"; }
+};
+}  // namespace dsa
+
+#endif  // !INCLUDE_RING_BUFFER_RING_BUFFER_H_
--- tests/ring_buffer/ring_buffer_test.cc
+++ tests/ring_buffer/ring_buffer_test.cc
@@ -1,3 +1,11 @@
 #include <gtest/gtest.h>
+#include <ring_buffer/ring_buffer.h>
 
-TEST(RingBuffer, Demo) { EXPECT_EQ(0, 1); }
+#include <cstdint>
+
+namespace dsa {
+TEST(RingBuffer, Demo) {
+  dsa::RingBuffer<uint8_t, 8> buf = RingBuffer<uint8_t, 8>();
+  (void)buf;
+}
+}  // namespace dsa
