5822d2fc90fb9dd979f27e232785dc8a83ca7904
Zane Helton <zane99@me.com>
2026-04-14 17:47:51 -0400

    Update flake.nix with aarch64-linux

--- flake.nix
+++ flake.nix
@@ -1,21 +1,30 @@
 {
   description = "C++ dev env";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
     {
-      devShells.x86_64-linux.default =
-        let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-        in
-        pkgs.mkShell {
-          packages = with pkgs; [
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
             just
             cmake
             ninja
           ];
         };
-    };
+      }
+    );
 }
